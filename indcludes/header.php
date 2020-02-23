8bc2c3b9ffe0fb199d01e32a50f0c3fea594d415
elfreits <elfreits@gmail.com>
2020-02-23 20:53:42 +0100

    structure file update 3

---
+++ indcludes/header.php
@@ -0,0 +1,22 @@
+<?php
+<div id="header-website">
+    <div class="center-website">
+        <div class="logo-website">
+            <div class="logo-textbox-website">
+                <div class="logo-text-website">mery</div>
+                <div class="logo-text-website">art gallery</div>
+            </div>
+            <div id="nav-website">
+                <div class="button-nav-website"><a href="?page=main">główna</a></div>
+                <div class="button-nav-website"><a href="?page=images">obrazy</a></div>
+                <div class="button-nav-website"> wydarzenia</div>
+                <div class="button-nav-website">gadżety</div>
+                <div class="button-nav-website">kontakt</div>
+                <div class="button-nav-website">logowanie</div>
+                <div class="button-nav-website"><i class="fas fa-shopping-basket"></i><div class="basket-dot-website">1</div></div>
+            </div>
+
+            <div style="clear:both;"></div>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -9,63 +9,12 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 </head>
-<body>
-    <div id="header-website">
-        <div class="center-website">
-            <div class="logo-website">
-                <div class="logo-textbox-website">
-                    <div class="logo-text-website">mery</div>
-                    <div class="logo-text-website">art gallery</div>
-                </div>
-                <div id="nav-website">
-                    <div class="button-nav-website"><a href="?page=main">główna</a></div>
-                    <div class="button-nav-website"><a href="?page=images">obrazy</a></div>
-                    <div class="button-nav-website"> wydarzenia</div>
-                    <div class="button-nav-website">gadżety</div>
-                    <div class="button-nav-website">kontakt</div>
-                    <div class="button-nav-website">logowanie</div>
-                    <div class="button-nav-website"><i class="fas fa-shopping-basket"></i><div class="basket-dot-website">1</div></div>
-                </div>
-                
-                <div style="clear:both;"></div>
-            </div>
-        </div>
-    </div>
-
-    <?php 
+    <body>
+    <?php
+        include 'indcludes/footer.php';
         include 'indcludes/pages.php';
+        include 'indcludes/footer.php';
     ?>
-    <div id="footer">
-        <div class="center-website">
-            <div class="footer-box-website">
-                <div class="footer-header-website">Menu</div>
-                <div class="footer-buttons-website">główna</div>
-                <div class="footer-buttons-website">obrazy</div>
-                <div class="footer-buttons-website">gadżety</div>
-                <div class="footer-buttons-website">wydarzenia</div>
-            
-            </div>
-            <div class="footer-box-website">
-                <div class="footer-header-website">konta</div>
-                <div class="footer-buttons-website">logowanie</div>
-                <div class="footer-buttons-website">rejestracja</div>
-                <div class="footer-buttons-website">dane</div>
-            
-            </div>
-            <div class="footer-box-website">
-                <div class="footer-header-website">socialmedia</div>
-                <div class="footer-buttons-website">instagram</div>
-                <div class="footer-buttons-website">facebook</div>
-                <div class="footer-buttons-website">youtube</div>
-            
-            </div>
-            <div class="footer-box-website">
-                <div class="footer-header-website">kontakt</div>
-                <div class="footer-buttons-website">biuro@merygallery.pl</div>
-            </div>
-            <div style="clear:both;"></div>
-            <p class="footer-text-website">&copy; Mery Art Gallery 2020 Wszelkie prawa zastrzeżone.</p>
-        </div>
-    </div>
-</body>
+
+    </body>
 </html>
